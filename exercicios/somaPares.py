12d063b3c016cb54156402cf173f481cc458e624
Osvaldo Junior <oseneda@gmail.com>
2024-05-16 20:46:10 -0300

    Programa somaPares

---
+++ exercicios/somaPares.py
@@ -0,0 +1,22 @@
+# Soma pares a partir do número dado, se o número for impar,
+# começa do par subsequente.
+
+import sys
+
+numero: float
+resultado: int = 0
+x: int
+
+print("Informe um numero: ")
+numero = int(sys.stdin.readline())
+
+if numero % 2 != 0:
+  numero += 1
+
+x = numero
+
+while x < numero + 10:
+  resultado += x
+  x += 2
+
+print("O resultado é = ", resultado)
